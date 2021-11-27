1bd18c6518e4f782c46b8f2a32f5c7f2d1dbdd2c
insideplant <kami.10pearl@gmail.com>
2021-11-27 11:27:24 +0900

    first commit

---
+++ src/lib/firebase.ts
@@ -0,0 +1,27 @@
+
+import * as firebase from 'firebase';
+import "firebase/firestore";
+// import { Constants } from 'expo-constants';
+
+/* types */
+import { Shop } from '../types/shop';
+
+if (!firebase.apps.length) {
+  const firebaseConfig = {
+    "apiKey": "AIzaSyB4WwGeow3aWo70qna-L-jF0tGNKYRLS4I",
+    "authDomain": "shop-review-c626c.firebaseapp.com",
+    "databaseURL": "https://project-id.firebaseio.com",
+    "projectId": "shop-review-c626c",
+    "storageBucket": "shop-review-c626c.appspot.com",
+    "messagingSenderId": "1038804004773",
+    "appId": "1:1038804004773:web:7696dce0ab960b4632024c",
+    "measurementId": "G-333WDDGFS9"
+  };
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const getShops = async() => {
+  const snapshot = await firebase.firestore().collection("shops").get();
+  const shops = snapshot.docs.map(doc => doc.data() as Shop);
+  return shops
+}
